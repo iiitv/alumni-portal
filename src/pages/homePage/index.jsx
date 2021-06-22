1385c62be2fbe1afbda37705e3a2fb9f9ed42137
Keshav Agarwal <keshavagarwal1710@gmail.com>
2021-06-22 11:50:49 +0530

    create slider for home page

---
+++ src/pages/homePage/index.jsx
@@ -0,0 +1,10 @@
+import ImageSlider from '../../Components/slider/index'
+const HomePage = ()=>{
+    return (
+        <div>
+            <ImageSlider />
+        </div>
+    )
+}
+
+export default HomePage;
