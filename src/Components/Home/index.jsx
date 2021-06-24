74ccae74c305845f17947af412856cab13d30771
Darshan Hande <darshanhande11@gmail.com>
2021-06-24 22:20:52 +0530

    added Navbar

--- src/Components/Home/index.jsx
+++ src/Components/Home/index.jsx
@@ -1,9 +1,12 @@
 import Gallery from './Gallery/index'
 import ImageSlider from './Slider/index'
+import Navbar from './Navbar/index'
+import 'semantic-ui-css/semantic.min.css'
 
 const Home = ()=>{
     return (
         <div>
+            <Navbar />
             <ImageSlider />
             <Gallery />
         </div>
