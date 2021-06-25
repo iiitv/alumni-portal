8eee27b1249b9f5da8a3da95674684ddca14bcc7
Darshan Hande <darshanhande11@gmail.com>
2021-06-25 13:36:25 +0530

    changed font of the Nav elements

--- src/Components/Home/Navbar/index.jsx
+++ src/Components/Home/Navbar/index.jsx
@@ -1,5 +1,6 @@
 import React,{ useState } from 'react'
 import {Menu,Dropdown} from 'semantic-ui-react'
+import '../Navbar/Navbar.scss'
 
 const Navbar = () => {
     const [activeItem,setActiveItem] = useState("");
@@ -18,44 +19,56 @@ const Navbar = () => {
           active={activeItem === 'About Us'}
           onClick={()=>clickHandler("About Us")}
           className='nav-items'
-        />
+        >
+          About Us
+        </Menu.Item>
 
         <Menu.Item
           name='Meet the Alumni'
           active={activeItem === 'Meet the Alumni'}
           onClick={()=>clickHandler("Meet the Alumni")}
           className='nav-items'
-        />
+        >
+          Meet the Alumni
+        </Menu.Item>
 
         <Menu.Item
           name='News'
           active={activeItem === 'News'}
           onClick={()=>clickHandler("News")}
           className='nav-items'
-        />
+        >
+          News
+        </Menu.Item>
 
         <Menu.Item
           name='Events'
           active={activeItem === 'Events'}
           onClick={()=>clickHandler("Events")}
           className='nav-items'
-        />
+        >
+          Events
+        </Menu.Item>
 
         <Menu.Item
           name='Jobs'
           active={activeItem === 'Jobs'}
           onClick={()=>clickHandler("Jobs")}
           className='nav-items'
-        />
+        >
+          Jobs
+        </Menu.Item>
 
         <Menu.Item
           name='Blogs'
           active={activeItem === 'Blogs'}
           onClick={()=>clickHandler("Blogs")}
           className='nav-items'
-        />
+        >
+          Blogs
+        </Menu.Item>
 
-        <Dropdown item text='More'>
+        <Dropdown item text='More' className='nav-items'>
             <Dropdown.Menu>
                 <Dropdown.Item text='something more' />
                 <Dropdown.Item text='nothing more' />
