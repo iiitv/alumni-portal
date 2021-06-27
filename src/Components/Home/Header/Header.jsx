7d18e33e28ee31cf7f403f7055a1c5db883c01d7
Darshan Hande <darshanhande11@gmail.com>
2021-06-28 00:49:36 +0530

    Header made responsive to all devices

--- src/Components/Home/Header/Header.jsx
+++ src/Components/Home/Header/Header.jsx
@@ -12,32 +12,36 @@ const Header = () => {
         "asset/images/Home/HeaderNFooter/instagram.png",
     ];
     return (
-        <>
-            <a href="/">
+        <div className='header'>
+            <div className='left-flex'>
                 <div className='brand-logo'>
                     <img src={logo} alt={"logo"} className='brand-img'/>
                 </div>
-            </a>
-            <div className='brand'>
-                <div className='brand-name'>
-                    Alumni Portal, IIIT Vadodara   
-                </div>
-                <Divider fitted={true}/>
-                <div className='brand-name'>
-                    Building Relations
+                <div className='brand'>
+                    <div className='brand-name'>
+                        Alumni Portal, IIIT Vadodara   
+                    </div>
+                    <Divider fitted={true} className='brand-divider'/>
+                    <div className='brand-name'>
+                        Building Relations
+                    </div>
                 </div>
             </div>
-            <div className='social'>
-                <a href="/"><img src={ socialIcons[0] } alt="facebook" className="social-icon" /></a>
-                <a href="/"><img src={ socialIcons[1] } alt="twitter" className="social-icon" /></a>
-                <a href="/"><img src={ socialIcons[2] } alt="linkedin" className="social-icon" /></a>
-                <a href="/"><img src={ socialIcons[3] } alt="youtube" className="social-icon" /></a>
-                <a href="/"><img src={ socialIcons[4] } alt="instagram" className="social-icon" /></a>
+            
+            <div className='right-flex'>
+                <div className='social'>
+                    <a href="/"><img src={ socialIcons[0] } alt="facebook" className="social-icon" /></a>
+                    <a href="/"><img src={ socialIcons[1] } alt="twitter" className="social-icon" /></a>
+                    <a href="/"><img src={ socialIcons[2] } alt="linkedin" className="social-icon" /></a>
+                    <a href="/"><img src={ socialIcons[3] } alt="youtube" className="social-icon" /></a>
+                    <a href="/"><img src={ socialIcons[4] } alt="instagram" className="social-icon" /></a>
+                </div>
+                <button className='button' onClick="/">
+                    Sign Up/Login
+                </button>
             </div>
-            <button className='button' onClick="/">
-                Sign Up/Login
-            </button>
-        </>
+            
+        </div>
     )
 }
 
