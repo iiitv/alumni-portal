9af444f0d0f3811e523a702d36b805a097a361a0
Keshav Agarwal <keshavagarwal1710@gmail.com>
2021-07-15 13:32:20 +0530

    remove unused variable

--- src/services/firebase.js
+++ src/services/firebase.js
@@ -19,21 +19,21 @@ export const auth = firebase.auth();
 const provider = new firebase.auth.GoogleAuthProvider();
 export const signInWithGoogle = () => {
     auth.signInWithPopup(provider).then((result) => {
-        var credential = result.credential;
+        // var credential = result.credential;
         // This gives you a Google Access Token. You can use it to access the Google API.
-        var token = credential.accessToken;
+        // var token = credential.accessToken;
         // The signed-in user info.
         var user = result.user;
         console.log(user);
         // ...
     }).catch((error) => {
         // Handle Errors here.
-        var errorCode = error.code;
+        // var errorCode = error.code;
         var errorMessage = error.message;
         // The email of the user's account used.
-        var email = error.email;
+        // var email = error.email;
         // The firebase.auth.AuthCredential type that was used.
-        var credential = error.credential;
+        // var credential = error.credential;
         console.log(errorMessage);
     });
 }
