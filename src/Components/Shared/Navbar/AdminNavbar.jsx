5d1fbec16332db98a08e71863f3e261c3d5092fd
nlok5923 <nlokhande5923@gmail.com>
2021-07-25 14:26:26 +0530

    partially fixed admin navbar issue

--- src/Components/Shared/Navbar/AdminNavbar.jsx
+++ src/Components/Shared/Navbar/AdminNavbar.jsx
@@ -1,25 +1,12 @@
-import React, { useState } from "react";
+import React, { useState, useLayoutEffect, useEffect } from "react";
 import { Menu, Icon, Sidebar } from "semantic-ui-react";
 import "../Navbar/Navbar.scss";
-import { createMedia } from "@artsy/fresnel";
 import { Link } from "react-router-dom";
 
 const navStyle = {
   backgroundColor: "#18535B",
   borderRadius: "0px",
 };
-
-const AppMedia = createMedia({
-  breakpoints: {
-    mobile: 320,
-    tablet: 768,
-    computer: 992,
-    largeScreen: 1200,
-    widescreen: 1920,
-  },
-});
-
-const { Media } = AppMedia;
 
 const items = [
   {content: "Home", key: "Home", link: "/"}, 
@@ -32,8 +19,29 @@ const items = [
   { content: "Gallery", key: "Gallery", link: "/admin/gallery" },
 ];
 
-const NavBarMobile = (props) => {
+const AdminNavbar = (props) => {
   const [visible, setVisible] = useState(false);
+  const [size, setSize] = useState([0, 0]);
+  const [childrenMobile, setChildrenMobile] = useState(false);
+  const [childrenDesktop, setChildrenDesktop] = useState(true);
+  useLayoutEffect(() => {
+    const updateSize = () => {
+      setSize([window.innerWidth, window.innerHeight]);
+    }
+    window.addEventListener('resize', updateSize);
+    updateSize();
+    return () => window.removeEventListener('resize', updateSize);
+  }, []);
+
+  useEffect(() => {
+    if(size[0] < 700) { 
+      setChildrenDesktop(false);
+      setChildrenMobile(true);
+     } else {
+      setChildrenDesktop(true);
+      setChildrenMobile(false);
+     }
+  }, [size])
   const pushHandler = () => {
     if(visible)
       setVisible(false);
@@ -44,88 +52,64 @@ const NavBarMobile = (props) => {
     setVisible(false);
   };
   const toggleHandler = () => setVisible(!visible);
+
   return (
-    <>
-      <Menu inverted style={navStyle} widths={4}>
-          <Menu.Item onClick={toggleHandler}>
-            <Icon name="sidebar" />
-          </Menu.Item>
-        </Menu>
-      <Sidebar.Pushable className='side-push'>
-        <Sidebar
-          as={Menu}
-          animation="overlay"
-          direction="top"
-          icon="labeled"
-          inverted
-          style={navStyle}
-          vertical
-          visible={visible}
-        >
+    <div>
+      <div className="desktop-nav">
+        <Menu inverted widths={11} style={navStyle}>
           {items.map((item) => (
-            <Menu.Item {...item} 
+            <Menu.Item
+              {...item}
               name={item}
               active={activeItem === item.content}
               onClick={() => clickHandler(item)}
               className="nav-items"
-              as = { Link }
-              to = {item.link}
-            >
+              as={Link}
+              to={item.link}
+            ></Menu.Item>
+          ))}
+        </Menu>
+        {(childrenDesktop) ? props.children : null }
+      </div>
+      <div className="mobile-nav">
+        <Menu inverted style={navStyle} widths={4}>
+            <Menu.Item onClick={toggleHandler}>
+              <Icon name="sidebar" />
             </Menu.Item>
-        ))}
-        </Sidebar>
-        <Sidebar.Pusher
-          dimmed={visible}
-          onClick={pushHandler}
-          style={{ minHeight: "100vh" }}
-          className='side-push'
-        >
-          {props.children}
-        </Sidebar.Pusher>
-      </Sidebar.Pushable>
-    </>
-  );
-};
-
-const NavBarDesktop = (props) => {
-  const [activeItem, setActiveItem] = useState("");
-  const clickHandler = (item) => {
-    setActiveItem(item.content);
-  };
-  return (
-    <>
-      <Menu inverted widths={11} style={navStyle}>
-        {items.map((item) => (
-          <Menu.Item
-            {...item}
-            name={item}
-            active={activeItem === item.content}
-            onClick={() => clickHandler(item)}
-            className="nav-items"
-            as={Link}
-            to={item.link}
-          ></Menu.Item>
-        ))}
-      </Menu>
-      {props.children}
-    </>
-  );
-};
-
-const AdminNavbar = (props) => {
-  return (
-    <div>
-      <Media at="mobile">
-        <NavBarMobile>
-            {props.children}
-        </NavBarMobile>
-      </Media>
-
-      <Media greaterThan="mobile">
-        <NavBarDesktop>
-            {props.children}
-        </NavBarDesktop>
-      </Media>
+          </Menu>
+        <Sidebar.Pushable className='side-push'>
+          <Sidebar
+            as={Menu}
+            animation="overlay"
+            direction="top"
+            icon="labeled"
+            inverted
+            style={navStyle}
+            vertical
+            visible={visible}
+          >
+            {items.map((item) => (
+              <Menu.Item {...item} 
+                name={item}
+                active={activeItem === item.content}
+                onClick={() => clickHandler(item)}
+                className="nav-items"
+                as = { Link }
+                to = {item.link}
+              >
+              </Menu.Item>
+          ))}
+          </Sidebar>
+          <Sidebar.Pusher
+            dimmed={visible}
+            onClick={pushHandler}
+            style={{ minHeight: "100vh" }}
+            className='side-push'
+          >
+            {(childrenMobile) ? props.children : null }
+          </Sidebar.Pusher>
+        </Sidebar.Pushable>
+      </div>
     </div>
   );
 };
