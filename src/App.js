30d3ec9b5e8c1f99c97e695abfbf7cdc5e3e780a
Ayush Patel <in.ayushpatel@gmail.com>
2021-07-27 15:26:39 +0530

    alimni dir admin

--- src/App.js
+++ src/App.js
@@ -31,6 +31,7 @@ import NewsAdminCard from "./Components/Admin/News/News/NewsDashboard";
 import BlogsAdminCard from "./Components/Admin/Blog/BlogDashboard/BlogDashboard";
 import AdminNewsPost from "./Components/Admin/News/NewsPost/NewsPost";
 import AdminBlogPost from "./Components/Admin/Blog/BlogPost/BlogPost";
+import AdminAumniCard from "./Components/Admin/Alumni/AlumniCard/AlumniCard";
 
 const AppMedia = createMedia({
   breakpoints: {
@@ -74,6 +75,7 @@ const App = () => {
                 <Route exact path="/admin/news/:id" component={AdminNewsPost} />
                 <Route exact path="/admin/blogs" component={BlogsAdminCard} />
                 <Route exact path="/admin/blogs/:id" component={AdminBlogPost} />
+                <Route exact path="/admin/alumni" component={AdminAumniCard} />
                 <Route component={Error404} />
               </Switch>
             </NavDecider>
